455d76142a17deed78ad54d75bffebb60fc2ba8d
Andrew Cole <andrew.cole@illallangi.com>
2013-05-22 13:10:56 +1000

    Initial Commit

---
+++ Illallangi.NullLogger/NullLogger.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using Ninject.Extensions.Logging;
+
+namespace Illallangi
+{
+    public sealed class NullLogger : LoggerBase
+    {
+        public NullLogger(Type type)
+            : base(type)
+        {
+        }
+
+        public NullLogger(string name)
+            : base(name)
+        {
+        }
+
+        public override void Debug(string message)
+        {
+            //noop
+        }
+
+        public override void Debug(string format, params object[] args)
+        {
+            //noop
+        }
+
+        public override void Debug(Exception exception, string format, params object[] args)
+        {
+            //noop
+        }
+
+        public override void DebugException(string message, Exception exception)
+        {
+            //noop
+        }
+
+        public override void Info(string message)
+        {
+            //noop
+        }
+
+        public override void Info(string format, params object[] args)
+        {
+            //noop
+        }
+
+        public override void Info(Exception exception, string format, params object[] args)
+        {
+            //noop
+        }
+
+        public override void InfoException(string message, Exception exception)
+        {
+            //noop
+        }
+
+        public override void Trace(string message)
+        {
+            //noop
+        }
+
+        public override void Trace(string format, params object[] args)
+        {
+            //noop
+        }
+
+        public override void Trace(Exception exception, string format, params object[] args)
+        {
+            //noop
+        }
+
+        public override void TraceException(string message, Exception exception)
+        {
+            //noop
+        }
+
+        public override void Warn(string message)
+        {
+            //noop
+        }
+
+        public override void Warn(string format, params object[] args)
+        {
+            //noop
+        }
+
+        public override void Warn(Exception exception, string format, params object[] args)
+        {
+            //noop
+        }
+
+        public override void WarnException(string message, Exception exception)
+        {
+            //noop
+        }
+
+        public override void Error(string message)
+        {
+            //noop
+        }
+
+        public override void Error(string format, params object[] args)
+        {
+            //noop
+        }
+
+        public override void Error(Exception exception, string format, params object[] args)
+        {
+            //noop
+        }
+
+        public override void ErrorException(string message, Exception exception)
+        {
+            //noop
+        }
+
+        public override void Fatal(string message)
+        {
+            //noop
+        }
+
+        public override void Fatal(string format, params object[] args)
+        {
+            //noop
+        }
+
+        public override void Fatal(Exception exception, string format, params object[] args)
+        {
+            //noop
+        }
+
+        public override void FatalException(string message, Exception exception)
+        {
+            //noop
+        }
+
+        public override bool IsDebugEnabled
+        {
+            get { return true; }
+        }
+
+        public override bool IsInfoEnabled
+        {
+            get { return true; }
+        }
+
+        public override bool IsTraceEnabled
+        {
+            get { return true; }
+        }
+
+        public override bool IsWarnEnabled
+        {
+            get { return true; }
+        }
+
+        public override bool IsErrorEnabled
+        {
+            get { return true; }
+        }
+
+        public override bool IsFatalEnabled
+        {
+            get { return true; }
+        }
+    }
+}
---
+++ Illallangi.NullLogger/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="AssemblyInfo.cs" company="Illallangi Enterprises">Copyright © 2013 Illallangi Enterprises</copyright>
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Illallangi Null Logger")]
+[assembly: AssemblyDescription("Illallangi Null Logger")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Illallangi Enterprises")]
+[assembly: AssemblyProduct("Illallangi Null Logger")]
+[assembly: AssemblyCopyright("Copyright © Illallangi Enterprises 2013")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d2a18e0a-a448-4ba4-b68f-f4850fcd702b")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
